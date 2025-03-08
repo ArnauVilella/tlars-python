d60ae4379add579356f49672a4746196baebe28c
ArnauVilella <avp@connect.ust.hk>
2025-03-09 04:02:57 +0800

    Update minimum Python version to 3.6 in setup.py

--- setup.py
+++ setup.py
@@ -222,7 +222,7 @@ setup(
     cmdclass={'build_ext': BuildExt},
     packages=['tlars'],
     zip_safe=False,
-    python_requires='>=3.8',
+    python_requires='>=3.6',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
