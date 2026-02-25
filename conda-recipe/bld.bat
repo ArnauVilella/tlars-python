293d8a45e6a16a74251596a13cb55ef4ba6bf710
ArnauVilella <avp@connect.ust.hk>
2026-02-25 22:27:15 +0800

    v0.7.14

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -3,18 +3,8 @@
 set "CPLUS_INCLUDE_PATH=%LIBRARY_INC%;%CPLUS_INCLUDE_PATH%"
 set "LIBRARY_PATH=%LIBRARY_LIB%;%LIBRARY_PATH%"
 
-REM Point setup.py at conda-provided armadillo and BLAS/LAPACK
 set "ARMADILLO_INCLUDE_DIR=%LIBRARY_INC%"
 set "ARMADILLO_LIB_DIR=%LIBRARY_LIB%"
-
-REM Debug: list available library files to diagnose BLAS/LAPACK linking
-echo "=== Available .lib files in %LIBRARY_LIB% ==="
-dir "%LIBRARY_LIB%\*.lib" 2>nul
-echo "=== MKL-related .lib files ==="
-dir "%LIBRARY_LIB%\mkl*.lib" 2>nul
-dir "%LIBRARY_LIB%\blas*.lib" 2>nul
-dir "%LIBRARY_LIB%\lapack*.lib" 2>nul
-echo "=== End library listing ==="
 
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
 if errorlevel 1 exit 1
