5c1d2c7efa07cc8dc827b0e3367b8504c86c29a0
ArnauVilella <avp@connect.ust.hk>
2026-02-26 02:36:02 +0800

    v0.8.0

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -3,7 +3,7 @@
 :: Remove vendored carma so the conda-forge host package is used instead
 if exist carma rmdir /s /q carma
 
-set "CPLUS_INCLUDE_PATH=%LIBRARY_INC%;%CPLUS_INCLUDE_PATH%"
+set "CPLUS_INCLUDE_PATH=%LIBRARY_INC%\carma;%LIBRARY_INC%;%CPLUS_INCLUDE_PATH%"
 set "LIBRARY_PATH=%LIBRARY_LIB%;%LIBRARY_PATH%"
 
 set "ARMADILLO_INCLUDE_DIR=%LIBRARY_INC%"
--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -7,8 +7,8 @@ set -ex
 # Remove vendored carma so the conda-forge host package is used instead
 rm -rf carma/
 
-# Set include paths for armadillo from conda-forge
-export CPLUS_INCLUDE_PATH="${PREFIX}/include:${CPLUS_INCLUDE_PATH}"
+# Set include paths from conda-forge
+export CPLUS_INCLUDE_PATH="${PREFIX}/include/carma:${PREFIX}/include:${CPLUS_INCLUDE_PATH}"
 export LIBRARY_PATH="${PREFIX}/lib:${LIBRARY_PATH}"
 
 # Build and install
