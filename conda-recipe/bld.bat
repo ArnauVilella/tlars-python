cdd70d2f2157d82844dce34af260623a7a817a5f
ArnauVilella <avp@connect.ust.hk>
2026-02-26 02:53:26 +0800

    v0.8.0

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -3,8 +3,8 @@
 :: Remove vendored carma so the conda-forge host package is used instead
 if exist carma rmdir /s /q carma
 
-set "CPLUS_INCLUDE_PATH=%LIBRARY_INC%\carma;%LIBRARY_INC%;%CPLUS_INCLUDE_PATH%"
-set "LIBRARY_PATH=%LIBRARY_LIB%;%LIBRARY_PATH%"
+set "INCLUDE=%LIBRARY_INC%\carma;%LIBRARY_INC%;%INCLUDE%"
+set "LIB=%LIBRARY_LIB%;%LIB%"
 
 set "ARMADILLO_INCLUDE_DIR=%LIBRARY_INC%"
 set "ARMADILLO_LIB_DIR=%LIBRARY_LIB%"
